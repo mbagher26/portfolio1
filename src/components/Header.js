bd17f1639d310d5d9dbd721fc264f60b769872a1
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-17 14:25:43 -0400

    Implement Header component with React Bootstrap Navbar and React Router Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header</div>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Link to='/home'>PROTFOLIO</Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <Link to='/login'></Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
