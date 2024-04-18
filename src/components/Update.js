d3f3a939b625616161691975da7f7c65ff92af8b
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 14:36:05 -0400

    Implement form submission and data updating functionality

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import supabase from '../SupabaseClient';
 import './../styles/Form.css'
 
@@ -11,9 +11,30 @@ function Update() {
           const [price, setPrice] = useState('');
           const [formError, setFormError] = useState(null);
           const { id } = useParams()
+          const navigate = useNavigate()
          
           const submitHandler = (e) => {
                     e.preventDefault()
+                    if (!name || !price) {
+                              setFormError('fill all field')
+                              return
+                    }
+
+                   const updating = async () => {
+                              const { data, errore } = await supabase
+                                        .from("products")
+                                        .update([{ name, price }])
+                                        .eq('id', id)
+                              if (errore) {
+                                        console.log(errore);
+                              }
+                              if (data) {
+                                        console.log(data);
+                              }
+                             navigate('/home', {replace: true})
+                   }
+                    updating()
+
           }
 
           useEffect( () => {
