2786dcb728844408977dd340699c5963eae8fe72
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 12:29:50 -0400

    Refactor Update component to fetch and display product details for updating

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,19 +1,41 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import supabase from '../SupabaseClient';
 
 function Update() {
 
-          const [name, setName] = useState();
-          const [price, setPrice] = useState();
-          const [formError, setFormError] = useState();
-
+          const [name, setName] = useState('');
+          const [price, setPrice] = useState('');
+          const [formError, setFormError] = useState(null);
+          const { id } = useParams()
          
+          const submitHandler = (e) => {
+                    e.preventDefault()
+          }
 
+          useEffect( () => {
+                    const fetchdata = async() => {
+                              const { data, error } = await supabase
+                                        .from("products")
+                                        .select()
+                                        .eq('id', id)
+                              if (data) {
+                                        setName(data[0].name)
+                                        setPrice(data[0].price)
+                              }
+                              if (error) {
+                                        console.log(error)
+                              }
+                    }
+                    fetchdata()
+          },[])
 
           return (
                     <div className='updata-container'>
                               <form className='update-form' onSubmit={submitHandler}>
-                                        <h1>Create new product</h1>
+                                        <h1>Update product{id }</h1>
                                         <label htmlFor="inputName">Name</label>
                                         <input id='inputName' placeholder='name' value={name} onChange={(e) => setName(e.target.value)} type="text" />
                                         <label htmlFor="inputPrice">Price</label>
