291553d7e9a4f6bd197898a6c4c0d32e014a926a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 07:10:17 -0400

    Add styling to Product component using external CSS file

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React from 'react'
+import './../styles/Product.css'
 
 const Product = (props) => {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className='product-card' style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>{ props.name }</Card.Title>
