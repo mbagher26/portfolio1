ee7897e3e42a2ee12d192e6e60920ad8f3372649
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 10:15:59 -0400

    add dom and state

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import { useState } from 'react'
 
 function Update() {
 
+          const [name, setName] = useState();
+          const [price, setPrice] = useState();
+          const [formError, setFormError] = useState();
+
+         
+
+
           return (
-                    <>UPDATE</>
+                    <div className='updata-container'>
+                              <form className='update-form' onSubmit={submitHandler}>
+                                        <h1>Create new product</h1>
+                                        <label htmlFor="inputName">Name</label>
+                                        <input id='inputName' placeholder='name' value={name} onChange={(e) => setName(e.target.value)} type="text" />
+                                        <label htmlFor="inputPrice">Price</label>
+                                        <input id='inputPrice' placeholder='price' value={price} onChange={(e) => setPrice(e.target.value)} type="text" />
+                                        <input type='submit' value="Update" />
+                              </form>
+                              {formError && <p>{formError}</p>}
+                    </div>
           )
 }
 
