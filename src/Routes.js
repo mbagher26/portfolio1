6cae61ee4f8b067cc3705279d2c6f60e5d7a755d
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 12:18:30 -0400

    add route for Update component

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,10 @@
 import Create from "./components/Create"
 import Home from "./components/Home"
+import Update from './components/Update'
 
 let Routes = [
           { path: '/home', element: <Home /> },
-          { path: '/create', element: <Create/>}
+          { path: '/create', element: <Create /> },
+          { path: '/update/:id' , element:<Update/>}
 ]
 export default Routes
