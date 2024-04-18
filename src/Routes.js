619e17c43eb49afb5adfea1e37e0c6d09f8897d6
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-18 08:58:21 -0400

    Add Create component route to Routes array

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
+import Create from "./components/Create"
 import Home from "./components/Home"
 
 let Routes = [
-          { path:'/home' , element: <Home/>},
+          { path: '/home', element: <Home /> },
+          { path: '/create', element: <Create/>}
 ]
 export default Routes
