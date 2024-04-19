60184693a23805149edd1e9c1bcffec812f9f97f
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-19 04:50:27 -0400

    Added functionality to delete products & Imported Supabase client for database operations. & Added delete button and functionality.

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,19 +3,45 @@ import React from 'react'
 import './../styles/Product.css'
 import { Link } from 'react-router-dom';
 import { BsPen } from "react-icons/bs";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import supabase from '../SupabaseClient';
 
 const Product = (props) => {
+
+  
+
+    const fetchdDelete = async () => {
+      const { data, error } = await supabase
+      
+      .from("products")
+      .delete()
+      .eq('id', props.id)
+      
+      if (error) {
+        console.log(error);
+      }
+      if (data) {
+        console.log(data);
+      }
+      props.onRemove(props.id)
+    }
+    
+  
+
   return (
     <Card className='product-card' style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>{ props.name }</Card.Title>
+        <Card.Title>{props.name}</Card.Title>
         <Card.Text>
           {props.price}
         </Card.Text>
         <Link to={`/update/${props.id}`}>
-         <BsPen/>updateing
+          <BsPen />updateing
         </Link>
+        <div className='btn-delete' onClick={fetchdDelete}>
+          <MdOutlineDeleteOutline />delete
+        </div>
       </Card.Body>
     </Card>
   )
