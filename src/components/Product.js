a4ad72a5d8b8c600ef2292deaf2ebc39d315590e
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-20 13:24:46 -0400

    delete Card.Img

--- src/components/Product.js
+++ src/components/Product.js
@@ -30,7 +30,6 @@ const Product = (props) => {
 
   return (
     <Card className='product-card' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>
